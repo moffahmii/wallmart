bfab909a7d00bafa5ded574ac665151eb7df0ce4
Mohamed Fahmy <fahmymohamed300@gmail.com>
2026-01-29 16:18:32 +0200

    s

--- src/components/addtoCart/addToCart.tsx
+++ src/components/addtoCart/addToCart.tsx
@@ -2,7 +2,7 @@
 import React, { useContext, useState } from 'react'
 import { CardFooter } from '../ui/card'
 import { Button } from '../ui/button'
-import { HeartIcon, Loader, ShoppingCartIcon } from 'lucide-react'
+import { Loader, ShoppingCartIcon } from 'lucide-react'
 import { toast } from 'sonner'
 import { CartContext } from '../context/cartContext'
 import { addToCartAction } from '@/app/(pages)/products/_action/addToCart.action'
@@ -12,14 +12,23 @@ import { useSession } from 'next-auth/react'
 export default function AddToCart({ productId }: { productId: string }) {
     const router = useRouter()
     const session = useSession()
-    let { getCart, setCartContent } = useContext(CartContext)
+    
+    const cartContext = useContext(CartContext)
+    if (!cartContext) {
+        throw new Error("CartContext is undefined. Make sure your component is wrapped in CartContextProvider")
+    }
+    const { getCart, setCartContent } = cartContext
+
     const [isLoading, setIsLoading] = useState(false)
+
     async function addProductToCart() {
         if (session.status == 'authenticated') {
             setIsLoading(true)
             const data = await addToCartAction(productId)
-            data.status == 'success' && toast.success('Product added successfully')
-            setCartContent(data)
+            if (data.status === 'success') {
+                toast.success('Product added successfully')
+                setCartContent(data)
+            }
             setIsLoading(false)
         } else {
             router.push('/login')
@@ -29,7 +38,7 @@ export default function AddToCart({ productId }: { productId: string }) {
     return (
         <CardFooter className='gap-2 mt-2'>
             <Button onClick={addProductToCart} className='grow cursor-pointer'>
-                {isLoading ? <Loader className='animate-spin' /> : <ShoppingCartIcon />}  Add to cart
+                {isLoading ? <Loader className='animate-spin' /> : <ShoppingCartIcon />} Add to cart
             </Button>
         </CardFooter>
     )
